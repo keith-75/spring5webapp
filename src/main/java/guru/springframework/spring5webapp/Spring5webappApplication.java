5a37b5c5b2a22038c17dd23198e9fe28743fbf8b
keith-75 <krshenderson75@yahoo.com>
2020-06-20 11:42:52 +0100

    devspring5webapp-1

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 
 @SpringBootApplication
 public class Spring5webappApplication {
